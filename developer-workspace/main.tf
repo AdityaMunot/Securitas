a746637a0614e478ab5b590c403ae89bdc6bc5a9
Aditya Munot <munot4@gmail.com>
2020-06-17 13:01:36 -0400

    Initial Template for Developer

---
+++ developer-workspace/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+provider "vault" {
+    address         = var.vault_address
+    token           = var.developer_token
+    skip_tls_verify = "true"
+    auth_login {
+        path = "auth/userpass/login/${var.login_username}"
+
+        parameters = {
+        password = var.login_password
+        }
+    }
+}
+
+data "vault_aws_access_credentials" "creds" {
+  backend = var.aws_backend
+  role    = var.aws_role
+}
+
+provider "aws" {
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# Create AWS EC2 Instance
+resource "aws_instance" "main" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name  =  "${var.name}"
+    TTL   =  "${var.ttl}"
+    owner =  "${var.name}-guide"
+  }
+}
---
+++ developer-workspace/variables.tf
@@ -0,0 +1,49 @@
+variable "vault_address" {
+  description = ""
+  type        = string
+  default     = ""
+}
+
+variable "developer_token" {
+  description = ""
+  type        = string
+  default     = ""
+}
+
+variable "login_username" {
+  description = ""
+  type        = string
+  default     = ""
+}
+
+variable "login_password" {
+  description = ""
+  type        = string
+  default     = ""
+}
+
+variable "aws_backend" {
+    description = ""
+    type        = string
+    default     = ""
+}
+
+variable "aws_role" {
+    description = ""
+    type        = string
+    default     = ""
+}
+
+
+variable "name" {
+  description = ""
+  type        = string
+  default     = ""
+}
+
+variable "ttl" {
+  description = ""
+  type        = string
+  default     = ""
+}
+
